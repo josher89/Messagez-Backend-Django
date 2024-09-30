4fed61e477da0c0d9d5d02b9339250070fb3aee1
Josher89 <j.hernandezruiz89@gmail.com>
2024-09-30 11:17:20 -0700

    Firts commit

---
+++ messagez/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Message
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ['id', 'text', 'timestamp']
+
+        def validate_text(self, value):
+            if Message.objects.filter(text=value).exists():
+                raise serializers.ValidationError("Duplicate message detected")
+            return value
---
+++ messagez/views.py
@@ -0,0 +1,9 @@
+#from django.shortcuts import render
+from rest_framework import generics
+from .models import Message
+from .serializers import MessageSerializer
+
+class MessageListCreateView(generics.ListCreateAPIView):
+    queryset = Message.objects.all().order_by('-timestamp')
+
+    serializer_class = MessageSerializer
